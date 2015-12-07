e085120fac413f1f6aa959e2986a88d1e36bde71
Mike Kane <mike.kane@students.makeschool.com>
2015-12-07 13:15:53 -0800

    initial commit

---
+++ PortfolioTracker/PortfolioTracker/customButtonView.swift
@@ -0,0 +1,23 @@
+//
+//  customButtonView.swift
+//  PortfolioTracker
+//
+//  Created by Mike Kane on 12/6/15.
+//  Copyright © 2015 Mike Kane. All rights reserved.
+//
+
+import UIKit
+
+class customButtonView: UIView {
+    
+    
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
